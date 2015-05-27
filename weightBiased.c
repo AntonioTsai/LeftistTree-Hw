2bf38e6dc89cbe477c6df8890ab11a479c5885d7
AntonioTsai <jimmyjack7777@gmail.com>
2015-05-28 01:24:56 +0800

    delete unnecessary files

--- weightBiased.c
+++
@@ -1,9 +0,0 @@
-//
-//  weightBiased.c
-//  LeftistTree
-//
-//  Created by Antonio Tsai on 2015/5/24.
-//
-#include <stdio.h>
-#include "weightBiased.h"
-
--- weightBiased.h
+++
@@ -1,7 +0,0 @@
-//
-//  weightBiased.h
-//  LeftistTree
-//
-//  Created by Antonio Tsai on 2015/5/24.
-//
-#include <stdio.h>
